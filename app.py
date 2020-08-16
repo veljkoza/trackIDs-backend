43be460ffb08e11712d06e223d9a4566c89a6216
veljkoza <veljko.sekulic1998@gmail.com>
2020-08-16 16:24:18 +0200

    bad version with request for shazam not passing 406 not acceptable(works in postman)

--- app.py
+++ app.py
@@ -5,7 +5,10 @@ from flask import request
 from flask_cors import CORS, cross_origin
 from pytube import YouTube
 from pydub import AudioSegment
+import http.client
+import mimetypes
 import base64
+import json
 AudioSegment.converter = "C:\\ffmpeg\\bin\\ffmpeg.exe"
 
 app = Flask(__name__)
@@ -36,21 +39,62 @@ def index():
 def returnLink(ytlink):
     link = request.args.get("ytlink")
     yt = YouTube("https://www.youtube.com/"+ytlink)
-    print(ytlink)
-    print(yt.streams.filter(only_audio=True))
     ys = yt.streams.filter(only_audio=True)[0]
     ys.download("songs")
-    # song = AudioSegment.from_file("Pelada A mí me juzgan por ser mujer.mp4","mp4")
+    song = AudioSegment.from_file(
+        "songs\XXXTENTACION - Jocelyn Flores (Audio).mp4", "mp4")
+    third_of_song = song.duration_seconds//3
 
-    # first_10_seconds = song[:ten_seconds]
-    # print("SOOOONG" % first_10_seconds)
-    # with open("Pelada A mí me juzgan por ser mujer.mp4", "rb") as audioFile:
-    #     text = base64.b64encode(audioFile.read())
-    #     file = open("song.txt", "wb")
-    # file.write(text)
-    # file.close()
-    return "nice"
-    # print(link)
+    left_four_seconds = (third_of_song-2) * 1000
+    right_four_seconds = (third_of_song+2) * 1000
+
+    eight_seconds_slice = song[left_four_seconds:right_four_seconds]
+    eight_seconds_slice = eight_seconds_slice.split_to_mono()[0]
+    eight_seconds_slice.set_frame_rate(44100)
+    eight_seconds_slice.set_sample_width(2)
+    eight_seconds_slice.export("slicedsong.mp3", format="mp3")
+
+    print("channels---------------", eight_seconds_slice.channels)
+    print("frame rate---------------", eight_seconds_slice.frame_rate)
+    print("sample_width---------------", eight_seconds_slice.sample_width)
+
+    raw_data = AudioSegment.from_mp3("slicedsong.mp3")._data
+    print("TYPE---------------", type(raw_data))
+
+    raw_text = base64.b64encode(raw_data).decode('UTF-8')
+
+    f = open("raw-text.txt", "w")
+    f.write(raw_text)
+
+    # text = base64.b64encode(raw_data)
+
+    with open("slicedsong.mp3", "rb") as audioFile:
+        text = base64.b64encode(audioFile.read())
+        file = open("song.txt", "wb")
+        # file.write(raw_text)
+        file.close()
+    print(song)
+    conn = http.client.HTTPSConnection("shazam.p.rapidapi.com")
+
+    payload = raw_text
+
+    headers = {
+        'x-rapidapi-host': "shazam.p.rapidapi.com",
+        'x-rapidapi-key': "b3071fb829msha09abfd4c5a24dep19a5b1jsne43ce7980b41",
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36',
+        'content-type': "text/plain",
+        'accept': "text/plain"
+    }
+
+    conn.request("POST", "/songs/detect", payload, headers)
+
+    res = conn.getresponse()
+    data = res.read()
+
+    print("RESPONSE--------------------------------", data.decode("utf-8"))
+    # json_data = response.json()
+    print("TYPE---------------", type(payload))
+    return raw_text
 
 
 @app.route("/pydub")
@@ -66,16 +110,15 @@ def editSong():
     # eight_seconds_slice.set_channels(1)
     eight_seconds_slice.set_frame_rate(44100)
     eight_seconds_slice.set_sample_width(2)
-    eight_seconds_slice.export("slicedsong.mp3",format="mp3")
+    eight_seconds_slice.export("slicedsong.mp3", format="mp3")
 
     print("channels---------------", eight_seconds_slice.channels)
     print("frame rate---------------", eight_seconds_slice.frame_rate)
     print("sample_width---------------", eight_seconds_slice.sample_width)
 
-
     raw_data = AudioSegment.from_mp3("slicedsong.mp3")._data
     raw_text = base64.b64encode(raw_data)
-    f = open("raw-text.txt","wb")
+    f = open("raw-text.txt", "wb")
     f.write(raw_text)
     f.close()
 
@@ -87,7 +130,18 @@ def editSong():
         file.write(raw_text)
         file.close()
     print(song)
-    return raw_text
+    url = "https://shazam.p.rapidapi.com/songs/detect"
+    payload = raw_text
+    headers = {
+        'x-rapidapi-host': "shazam.p.rapidapi.com",
+        'x-rapidapi-key': "b3071fb829msha09abfd4c5a24dep19a5b1jsne43ce7980b41",
+        'content-type': "text/plain",
+        'accept': "text/plain"
+    }
+
+    response = requests.request("POST", url, data=payload, headers=headers)
+
+    return response
 
 
 if __name__ == "__main__":
